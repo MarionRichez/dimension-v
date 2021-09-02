9c6b6911dcf6794b62c110a76873a29141feddc6
Marion Richez <richez.m95@gmail.com>
2021-09-02 16:51:33 +0200

    réparation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,13 @@
-import { Component } from '@angular/core';
-
-import {
-  Router,
-  Event as RouterEvent,
-  NavigationStart,
-  NavigationEnd,
-  NavigationCancel,
-  NavigationError,
-} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  public showLoader = true;
+export class AppComponent implements OnInit  {
+  constructor() { }
 
-  constructor(private router: Router) {
-    this.router.events.subscribe((event: RouterEvent) => {
-      this.navigationInterceptor(event);
-    });
-  }
-
-  navigationInterceptor(event: RouterEvent): void {
-    if (event instanceof NavigationStart) {
-      this.showLoader = true;
-    }
-
-    if (event instanceof NavigationEnd) {
-      this.showLoader = false;
-    }
-
-    if (event instanceof NavigationCancel) {
-      this.showLoader = false;
-    }
-
-    if (event instanceof NavigationError) {
-      this.showLoader = false;
-    }
+  ngOnInit(): void {
   }
 }
